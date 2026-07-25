a1e07f403715b3793cf8af1c1dd4e38217af7197
Axiomboy <vkpmom@gmail.com>
2026-07-25 12:03:52 -0700

    Implement write function for user input

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+write = print
+
+User_Code = input("Hello. This version 0.0.1 can only use the write function. Start coding")
+
+if User_Code[0:6] == " write":
+    write(User_Code [7:])
+else:
+    write("Nonexistent Function")
 
