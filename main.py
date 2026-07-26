bbb60b207f18998ceb26e34431f6a1eec267e9f2
Axiomboy <vkpmom@gmail.com>
2026-07-25 20:44:00 -0700

    Update input prompt message for clarity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 write = print
 
-User_Code = input("Hello. Version 0.0.1 can only use the write function. Start coding")
-
+User_Code = input("Hello. Version 0.0.1 can only use the write function. You can start coding")
 if User_Code[0:6] == " write":
     write(User_Code [7:])
 else:
