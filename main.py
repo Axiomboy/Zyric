da3abbc2fed662e3f6514505a51920abfba592ea
Axiomboy <vkpmom@gmail.com>
2026-07-27 14:10:23 -0700

    Fix conditional logic for echo command

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ User_Code = user("Hello. Version 0.0.1 can only use write, and input. You are ab
 
 if User_Code[0:5] == " echo":
     write(User_Code [6:])
-elif User_Code[0:5] == " echo" and User_Code[6:] == User_Input:
+elif User_Code[6:] == User_Input and User_Code[0:5] == " echo"
   print(User_Input) 
 elif User_Code[0:5] == " user":
     user(User_Code) == User_Input
