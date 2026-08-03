02dbb0fb303f34c373427e0c71e9f183466a8df4
Axiomboy <vkpmom@gmail.com>
2026-08-03 06:29:46 -0700

    Fix indentation for print statement

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ write = print
 USER_INPUT = input("hello. this version only uses write(). You can start coding.") 
 
 if USER_INPUT[ :6] == ' write':
-    print(USER_INPUT[7: ]
+    print(USER_INPUT[7: ])
