85bf1b880194917990fcd3cef4be18c63119ebe0
Axiomboy <vkpmom@gmail.com>
2026-08-03 06:26:50 -0700

    Update input handling and add echo functionality

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 write = print
 
-input("hello. this version only uses write()")
+input("hello. this version only uses write(). You can start coding.") = USER_INPUT
+
+if USER_INPUT[ :5] = ' echo':
+    print(USER_INPUT[6: ]
