a0f7d3c37c8ffd08df9b4c682c81cadbc890b22c
Axiomboy <vkpmom@gmail.com>
2026-08-03 06:27:12 -0700

    Change command from 'echo' to 'write' in main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ write = print
 
 input("hello. this version only uses write(). You can start coding.") = USER_INPUT
 
-if USER_INPUT[ :5] = ' echo':
+if USER_INPUT[ :6] = ' write':
     print(USER_INPUT[6: ]
