4fe42d7df0714b613bbf59b2d91b5bc1e773761a
Axiomboy <vkpmom@gmail.com>
2026-08-03 06:38:48 -0700

    Fix condition to check for 'write' input

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ write = print
 
 USER_INPUT = input("hello. this version only uses write(). You can start coding.") 
 
-if USER_INPUT[ :6] == ' write':
+if USER_INPUT[ :6] == ' write' or 'write':
     print(USER_INPUT[7: ])
 else:
     print("Invalid Function")
