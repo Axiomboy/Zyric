ee89bde1c3c4972a451bf8b6cc9b4777638d1666
Axiomboy <vkpmom@gmail.com>
2026-08-03 06:27:25 -0700

    Fix index error in print statement

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ write = print
 input("hello. this version only uses write(). You can start coding.") = USER_INPUT
 
 if USER_INPUT[ :6] = ' write':
-    print(USER_INPUT[6: ]
+    print(USER_INPUT[7: ]
